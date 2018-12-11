bdb1bbe39e0beab9d22b86b76be33c035403bf3f
Andy <drumsetmoneky@gmail.com>
2018-12-11 01:34:33 -0500

    Started hardfault handling

--- src/kernel/fault.c
+++ src/kernel/fault.c
@@ -1,8 +1,13 @@
+#include "stdint.h"
+
 void HardFault_Handler(void)
 {
+    // TODO output this value over serial for debugging
+    volatile uint32_t* location asm("r0");
+
     // loop forever and keep fault value in R0
     asm volatile(
-        "cpsid  I\t\n"
-        "b      .\t\n"
+        "cpsid  I\t\n"      // disable interrupts
+        "b      .\t\n"      // loop
     );
 }
