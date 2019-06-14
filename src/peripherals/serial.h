1c6520bbac228bc9e64a01c727e04f6c1a19b3a8
drumsetmonkey <drumsetmonkey@gmail.com>
2019-06-14 00:43:31 -0400

    Added support for serial communication over USB

---
+++ src/peripherals/serial.h
@@ -0,0 +1,32 @@
+/*
+ * @file serial.h
+ *
+ * Copyright (C) 2019  Belle-Isle, Andrew <drumsetmonkey@gmail.com>
+ * Author: Belle-Isle, Andrew <drumsetmonkey@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SERIAL_H
+#define SERIAL_H
+
+// STM LIBS
+#include "system/stm32h7xx.h"
+
+int Serial_Init(uint32_t, int);
+
+int PutChar(char);
+char GetChar();
+
+#endif //SERIAL_H
