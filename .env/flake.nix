def76cc2532ceca4f3e40f3c3b8876a4d5b431d6
abelleisle <abelleisle@protonmail.com>
2024-01-03 19:44:04 -0600

    Added 'exit' to flake.nix

--- .env/flake.nix
+++ .env/flake.nix
@@ -3,7 +3,7 @@
 
   inputs =
     {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
       flake-utils.url = "github:numtide/flake-utils";
     };
 
@@ -26,6 +26,7 @@
             ];
             shellHook = ''
               $SHELL
+              exit
             '';
           };
         }
